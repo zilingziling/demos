5a6b7727cde50ee04079f65086a73617cae5fc0b
ziling <805217189@qq.com>
2019-08-27 14:30:42 +0800

    改造motion页的布局为顶部侧边布局

---
+++ src/components/Sider/sider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Menu} from 'antd';
+
+const Sider=()=>{
+  return (
+    <h1>sider</h1>
+  )
+}
+export default Sider
--- src/pages/layout/motionLayout.js
+++ src/pages/layout/motionLayout.js
@@ -1,12 +1,27 @@
-import React  from 'react';
+import React from 'react';
+import { Layout, Menu } from 'antd';
+const { Header,Content,Sider } = Layout;
 
-
-const MotionLayout=()=>{
+const MotionLayout = () => {
   return (
-      <div>
-        {/*<Sider/>*/}
-        {/*<RightContent></RightContent>*/}
-      </div>
-  )
-}
-export default  MotionLayout
+    <Layout>
+      <Header>
+        <div>logo</div>
+        <Menu>
+          <Menu.Item>HOVER</Menu.Item>
+          <Menu.Item>MOVEMENT</Menu.Item>
+          <Menu.Item>PAGE</Menu.Item>
+        </Menu>
+      </Header>
+      <Content>
+        <Layout>
+          <Sider>
+            1234
+          </Sider>
+          <Content>124</Content>
+        </Layout>
+      </Content>
+    </Layout>
+  );
+};
+export default MotionLayout;
